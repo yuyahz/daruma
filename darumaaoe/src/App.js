7f7c8ad2ee05bed0dce8af831c4bf72719b89d70
Yuya Hashirizaki <hashirizaki61@gmail.com>
2020-09-09 23:27:59 -0700

    add logo

--- darumaaoe/src/App.js
+++ darumaaoe/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './daruma_logo.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          daruma aoe
         </p>
         <a
           className="App-link"
