25c470c92660304cce08077433f091d29b153719
Yuya Hashirizaki <hashirizaki61@gmail.com>
2020-09-11 16:56:14 -0700

    bit

--- darumaaoe/src/App.js
+++ darumaaoe/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          daruma aoe
+          daruma aoe ten
         </p>
         <a
           className="App-link"
