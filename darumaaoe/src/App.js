32055b9d7af9cf2c6356e976f2a4a48aad8e1a9d
Yuya Hashirizaki <hashirizaki61@gmail.com>
2020-09-27 02:25:04 -0700

    leaning router

--- darumaaoe/src/App.js
+++ darumaaoe/src/App.js
@@ -1,28 +1,37 @@
+// App.jsx
+
 import React from 'react';
-import logo from './daruma_logo.svg';
+import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p
-          className="title-font">
-          Japanese BBQ<br></br>
-          DARUMA<br></br>
-          AOE
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        // Set up the Router
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+
+            // Set up the Links
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- darumaaoe/src/Projects.js
+++ darumaaoe/src/Projects.js
@@ -1,11 +1,32 @@
-import React from "react"
+import React from 'react';
+import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
+import './App.css';
 
-function NewComponent(props) {
+function Projects(props) {
     return (
+        // Render a Thumbnail component
         <div>
             <h1>Projects</h1>
+            <Thumbnail
+                link="/twitter"
+                image="http://twitter-image-url.jpg"
+                title="Twitter Newsfeed"
+                category="Mobile App"
+            />
+            <Thumbnail
+                link="/airbnb"
+                image="http://airbnb-image-url.jpg>"
+                title="Airbnb Experiences"
+                category="Website"
+            />
+            <Thumbnail
+                link="/photoshop"
+                image="http://photoshop-image-url.jpg"
+                title="Photoshop Redesign"
+                category="Desktop App"
+            />
         </div>
     )
 }
 
-export default NewComponent;
+export default Projects;
