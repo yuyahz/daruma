3616e1b5335852486d1dadc03c73b080f6146c84
Yuya Hashirizaki <hashirizaki61@gmail.com>
2020-12-03 23:15:56 -0800

    menu

--- aoe/src/config/data.js
+++ aoe/src/config/data.js
@@ -1,46 +1,60 @@
 import FujiCard from "../assets/FujiCard.png";
+import Yakiniku from "../assets/Yakiniku.png";
+import OneDish from "../assets/OneDish.png";
+import RiceDishes from "../assets/RiceDishes.png";
+import Drink from "../assets/Drink.png";
+import Dessert from "../assets/Dessert.png";
+import PartyPlan from "../assets/PartyPlan.png";
+import Lunch from "../assets/Lunch.png";
 
 export const CardData = [
-    {
-        id: 'yakiniku',
-        cardImg: FujiCard,
-        cardTitle: "焼肉",
-        cardText: "市場直送の新鮮で選りすぐりの部位を 美味しく召し上がって頂けるように、 スタッフ自ら仕込み提供しています。",
-    },
-    {
-        id: 'one-dish',
-        cardImg: FujiCard,
-        cardTitle: "刺身・一品料理・生野菜",
-        cardText: "安心安全を第一にスタッフ自ら仕込んだ新鮮な牛の刺身と、当店自慢の本場 韓国料理の味を是非。",
-    },
-    {
-        id: 'rice-dishes',
-        cardImg: FujiCard,
-        cardTitle: "ご飯もの・スープ",
-        cardText: "先代から受け継がれたレシピは、牛から出汁をとりじっく手間暇かけたスー プです。",
-    },
-    {
-        id: 'drinks',
-        cardImg: FujiCard,
-        cardTitle: "お飲物",
-        cardText: "店長自らピックアップした日本と韓国両方のお酒を取り揃えています。",
-    },
-    {
-        id: 'dessert',
-        cardImg: FujiCard,
-        cardTitle: "デザート",
-        cardText: "一番人気のソフトクリーム以外にも、フレッシュなフルーツのアイスも取り 揃えています。",
-    },
-    {
-        id: 'party-plan',
-        cardImg: FujiCard,
-        cardTitle: "宴会コース料理",
-        cardText: "幹事さまの手間を省く為に分かりやすく、満足していただけるコースをご用 意しております。",
-    },
-    {
-        id: 'lunch',
-        cardImg: FujiCard,
-        cardTitle: "韓国ランチ",
-        cardText: "リーズナブルに本格韓国料理を楽しんで召し上がって頂けるメニューをご用 意しています。",
-    }
+  {
+    id: "yakiniku",
+    cardImg: Yakiniku,
+    cardTitle: "焼肉",
+    cardText:
+      "市場直送の新鮮で選りすぐりの部位を 美味しく召し上がって頂けるように、スタッフ自ら仕込み提供しています。",
+  },
+  {
+    id: "one-dish",
+    cardImg: OneDish,
+    cardTitle: "刺身・一品料理・生野菜",
+    cardText:
+      "安心安全を第一にスタッフ自ら仕込んだ新鮮な牛の刺身と、当店自慢の本場韓国料理の味を是非。",
+  },
+  {
+    id: "rice-dishes",
+    cardImg: RiceDishes,
+    cardTitle: "ご飯もの・スープ",
+    cardText:
+      "先代から受け継がれたレシピは、牛から出汁をとりじっく手間暇かけたスー プです。当店オリジナルのメニューも大人気!!",
+  },
+  {
+    id: "drinks",
+    cardImg: Drink,
+    cardTitle: "お飲物",
+    cardText:
+      "キンキンに冷えたビールは勿論!! 店長自らピックアップした日本と韓国両方のお酒を取り揃えています。",
+  },
+  {
+    id: "dessert",
+    cardImg: Dessert,
+    cardTitle: "デザート",
+    cardText:
+      "大人にもお子様にも人気のソフトクリーム以外にも、フレッシュフルーツのアイスも取り揃えています。",
+  },
+  {
+    id: "party-plan",
+    cardImg: PartyPlan,
+    cardTitle: "宴会コース料理",
+    cardText:
+      "幹事さまの手間を省く為に分かりやすく、満足していただけるコースをご用意しております。",
+  },
+  {
+    id: "lunch",
+    cardImg: Lunch,
+    cardTitle: "韓国ランチ",
+    cardText:
+      "リーズナブルに本格韓国料理を楽しんで召し上がって頂けるメニューをご用意しています。",
+  },
 ];
